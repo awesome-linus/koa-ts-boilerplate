3ae9e3c993e1819d88c631fbe71c3934047cb506
kou <contact@awesome-linus.com>
2021-09-03 13:42:20 +0900

    fix migration task

--- migrations/20210903043432_create_tasks_table.js
+++ migrations/20210903043432_create_tasks_table.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
-};
+    return knex.schema.createTable('tasks', function(table) {
+        table.increments();
+        table.string('title').notNullable();
+        table.string('description').notNullable();
+        table.boolean('is_complete').notNullable().defaultTo(false);
+        table.integer('user_id').references('id').inTable('users');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+}
 
 exports.down = function(knex) {
-  
-};
+    return knex.schema.dropTable('tasks');
+}
