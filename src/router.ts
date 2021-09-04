efa0873772348621657baa556377afbc120fa6a0
kou <contact@awesome-linus.com>
2021-09-04 10:44:41 +0900

    add users api

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,7 @@
 import Router from 'koa-router';
+import { ParsedUrlQuery } from 'querystring';
+import isUndefined from 'lodash/isUndefined';
+import { knex } from './knex';
 
 const router = new Router();
 
@@ -17,6 +20,33 @@ interface HelloRequest {
 router.post('/', async (ctx, next) => {
   const { name } = <HelloRequest>ctx.request.body;
   ctx.body = { name };
+  await next();
+});
+
+interface FetchUsersRequest extends ParsedUrlQuery{
+  first: string;
+  offset: string;
+  fields: string;
+}
+
+router.get('/api/users', async (ctx, next) => {
+  const { first, offset, fields } = <FetchUsersRequest>ctx.request.query;
+
+  const users = knex.from('users');
+
+  if (!isUndefined(fields) && fields !== '') {
+    users.select(fields?.split(',') as string[]);
+  } else {
+    users.select('*');
+  }
+  if (Number(offset) > 0) {
+    users.offset(Number(offset));
+  }
+  if (Number(first) > 0) {
+    users.limit(Number(first));
+  }
+  ctx.body = await users;
+
   await next();
 });
 
