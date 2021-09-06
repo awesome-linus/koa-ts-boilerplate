129864f58a17235bb50888df73143bd5bd7f1ecb
kou <contact@awesome-linus.com>
2021-09-07 02:21:33 +0900

    change ts knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,14 +1,11 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'mysql',
     connection: {
       host: '127.0.0.1',
       user: 'root',
       password: '',
-      database: 'koa_ts_knex'
+      database: 'koa_ts_knex',
     },
   },
 
@@ -16,32 +13,31 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: 'knex_migrations',
+    },
+  },
 };
